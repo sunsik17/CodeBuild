67680b92fcf48dab65163b097649bbc6611af873
sunsik <sunsig21c@gmail.com>
2023-07-17 00:38:58 +0900

    modify test controller - mapping endpoint

--- module-user/src/main/java/com/zerobase/user/TestController.java
+++ module-user/src/main/java/com/zerobase/user/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	@GetMapping
+	@GetMapping("ping")
 	public String ping() {
 		return "pong";
 	}
